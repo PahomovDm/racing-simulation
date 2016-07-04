3a3c0072da8f3eea2febc840978da7fc9e78ecb6
dpahomov <dpahomov@WS44-PC.milane.local>
2016-07-04 18:47:00 +0300

    add Checkstyle

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,48 +1,39 @@
 import java.util.Scanner;
 
-public class Test {
-	private static  String name;
-	private static  Scanner scan = new Scanner (System.in);
+class Test {
 
+	private static Scanner scan = new Scanner(System.in);
+	private static String name;
 	public static void main(String[] args) {
-		
-		up();
-		entry();
-		trimLine();
-		helloName();
-		equalsEnter();
+	up();
+	entry();
+	trimLine();
+	helloName();
+	equalsEnter();
 	}
-	
 	private static void up() {
-		System.out.println("Program rasing-simulation\n");
+	System.out.println("Program rasing-simulation\n");
 	}
-
-	private static void entry() {	
- 		System.out.println("Please enter your name:");
-		name = scan.nextLine();
+	private static void entry() {
+	System.out.println("Please enter your name:");
+	name = scan.nextLine();
 	}
-	
-	private static String trimLine () {
-		if (name == null) 
+	private static String trimLine() {
+		if (name == null) {
 			 System.out.println("Null");
-		
+		}
 		while (name.trim().equals("")) {
 			System.out.println("Empty");
-						
 			entry();
-		} 
-		
+		}
 		return name;
 	}
-	
-	private static void helloName () {
- 		System.out.printf("Hello %s! This is Racing Simulator\n", name);
+	private static void helloName() {
+ 		System.out.printf("Hello %s! This is Racing Simulator\n", name.trim());
 	}
-	
-	private static void equalsEnter () {
+	private static void equalsEnter() {
 		System.out.print("Press <Enter> to exit...");
 		while (("\n").equals(scan.nextLine()));
 
 	}
-	
 }
