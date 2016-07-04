57387b27b79af340ee65339ff9864a4e468bf1bc
dpahomov <dpahomov@WS44-PC.milane.local>
2016-07-04 15:01:52 +0300

    implemented methods trimLine() and equalsEnter()

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,23 +1,37 @@
 import java.util.Scanner;
 
 public class Test {
+	private static  String name;
+	private static  Scanner scan = new Scanner (System.in);
 
-	public static void main(String[] args) throws Exception {
-		Scanner scan = new Scanner (System.in);
-		String name;
+	public static void main(String[] args) {
 		
 		System.out.println("Please enter your name:");
-		do {
-			 name = scan.nextLine();
-		} while (name.trim().equals(""));
+		
+		trimLine();
 		
 		System.out.printf("Hello %s! This is Racing Simulator\n", name);
 		System.out.print("Press <Enter> to exit...");
 		
+		equalsEnter();
+	}
+	
+	static String trimLine ()
+	{
+		do {
+			 name = scan.nextLine();
+		} while (name.trim().equals(""));
+		
+		return name.trim();
+	}
+
+	static void equalsEnter ()
+	{
 		while (("\n").equals(scan.nextLine())) {
 		;
 		}
-		
+
 	}
+
 
 }
