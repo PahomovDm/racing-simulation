fc6d057e5fc51df80dcfec6d933112f6e40e3b97
dpahomov <dpahomov@WS44-PC.milane.local>
2016-07-05 11:55:38 +0300

    new name methods

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,39 +1,47 @@
 import java.util.Scanner;
+public final class Test {
 
-class Test {
+    private static Scanner scan = new Scanner(System.in);
+    private static String name;
 
-	private static Scanner scan = new Scanner(System.in);
-	private static String name;
-	public static void main(String[] args) {
-	up();
-	entry();
-	trimLine();
-	helloName();
-	equalsEnter();
-	}
-	private static void up() {
-	System.out.println("Program rasing-simulation\n");
-	}
-	private static void entry() {
-	System.out.println("Please enter your name:");
-	name = scan.nextLine();
-	}
-	private static String trimLine() {
-		if (name == null) {
-			 System.out.println("Null");
-		}
-		while (name.trim().equals("")) {
-			System.out.println("Empty");
-			entry();
-		}
-		return name;
-	}
-	private static void helloName() {
- 		System.out.printf("Hello %s! This is Racing Simulator\n", name.trim());
-	}
-	private static void equalsEnter() {
-		System.out.print("Press <Enter> to exit...");
-		while (("\n").equals(scan.nextLine()));
+    private Test() {
+    }
 
-	}
+    public static void main(String[] args) {
+        getInfo();
+        enterName();
+        trimLine();
+        helloName();
+        equalsEnter();
+    }
+
+    private static void getInfo() {
+        System.out.println("Program rasing-simulation\n");
+    }
+
+    private static void enterName() {
+        System.out.println("Please enter your name:");
+        name = scan.nextLine();
+    }
+
+    private static String trimLine() {
+        if (name == null) {
+            System.out.println("Null");
+        }
+        while (name.trim().equals("")) {
+            System.out.println("Empty");
+            enterName();
+        }
+        return name;
+    }
+
+    private static void helloName() {
+        System.out.printf("Hello %s! This is Racing Simulator\n", name.trim());
+    }
+
+    private static void equalsEnter() {
+        while (("\n").equals(scan.nextLine())) {
+            System.out.print("Press <Enter> to exit...");
+        }
+    }
 }
