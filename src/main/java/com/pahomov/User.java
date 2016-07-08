ee17e875d525829d8fe131a04293a055e87040b0
dpahomov <dpahomov@WS44-PC.milane.local>
2016-07-08 17:15:24 +0300

    new Class User

---
+++ src/main/java/com/pahomov/User.java
@@ -0,0 +1,20 @@
+package com.pahomov;
+
+import java.util.GregorianCalendar;
+
+class User {
+    private String name;
+    private GregorianCalendar birthday;
+
+    public User (int aName) {
+        
+    }
+    public void checkAge() {
+    };
+
+    public void readBirthDay() {
+    };
+
+    public boolean checkName() {
+    }
+}
