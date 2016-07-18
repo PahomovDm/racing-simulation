c49caacbac64e4bed64d821b4cf847319e345263
dpahomov <dpahomov@WS44-PC.milane.local>
2016-07-18 11:47:58 +0300

    Signed-off-by: dpahomov <dpahomov@WS44-PC.milane.local>

--- src/main/java/com/pahomov/RasingSimulationApp.java
+++ src/main/java/com/pahomov/RasingSimulationApp.java
@@ -1,20 +1,73 @@
 package com.pahomov;
 
-public final class RasingSimulationApp {
-    private RasingSimulationApp() {
+import com.pahomov.Exception.*;
+import com.pahomov.UserBuilder.*;
+import com.pahomov.greet.*;
 
-    };
+import java.time.DateTimeException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+import org.apache.log4j.Logger;
+
+/**
+ * @author dpahomov
+ *
+ */
+
+public class RasingSimulationApp {
+
+    public static final Logger LOG = Logger.getLogger(RasingSimulationApp.class);
+    private Scanner scan = new Scanner(System.in);
+    private CreateGreeter factory = new CreateGreeter();
+    // private Greeter greeter;
+    private List<User> users = new ArrayList<User>();
 
     public static void main(String[] args) {
-        RasingSimulationApp test = new RasingSimulationApp();
-        test.run();
+        RasingSimulationApp myTest = new RasingSimulationApp();
+        myTest.run();
+    }
+
+    private void getInfo() {
+        System.out.println("Program rasing-simulation\n");
+    }
+
+    private void closeProgram() {
+        do {
+            System.out.print("\nPress <Enter> to exit...");
+        } while (("\n").equals(scan.nextLine()));
+    }
+
+    private void inputUsers() {
+        do {
+            try {
+                users.add(new ConsoleUserBuilder().name().birthday().build());
+            } catch (AgeException ex) {
+                System.out.println(ex.getMessage());
+            } catch (DateTimeException ex) {
+                System.out.println(ex.getMessage());
+            } catch (NullNameException ex) {
+                System.out.println(ex.getMessage());
+            }
+            System.out.print("\nClick <0> to add");
+        } while (scan.nextInt() == 0);
+    }
+
+    private void outputUsers() {
+        Collections.sort(users);
+        users.stream().forEachOrdered(u -> factory.factoryGreeter(u).greetUser(u));
     }
 
     private void run() {
-        inputMachine();
+        LOG.info("Start");
+        try {
+            getInfo();
+            inputUsers();
+            outputUsers();
+        } finally {
+            closeProgram();
+            LOG.info("End");
+        }
     }
-
-    private void inputMachine() {
-    };
-
 }
